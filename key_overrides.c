0a5141f87ff8d74183254192d354ffae9bf26b36
inferno214221 <inferno214221@gmail.com>
2026-03-09 17:16:47 +1030

    Adds trial hash-dollar key.

--- key_overrides.c
+++ key_overrides.c
@@ -10,10 +10,10 @@ KEY_OVERRIDE_SHIFT_OR_COMBO(o_lcbr_1_none, o_lcbr_1_shift, KC_1, KC_LCBR);
 KEY_OVERRIDE_SHIFT_OR_COMBO(o_rcbr_2_none, o_rcbr_2_shift, KC_2, KC_RCBR);
 KEY_OVERRIDE_SHIFT_OR_COMBO(o_labk_3_none, o_dlr_3_shift,  KC_3, KC_LABK);
 KEY_OVERRIDE_SHIFT_OR_COMBO(o_rabk_4_none, o_labk_4_shift, KC_4, KC_RABK);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_dlr_5_none,  o_rabk_5_shift, KC_5, KC_DLR);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_lbrc_6_none, o_lbrc_6_shift, KC_6, KC_LBRC);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_rbrc_7_none, o_rbrc_7_shift, KC_7, KC_RBRC);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_hash_8_none, o_hash_8_shift, KC_8, KC_HASH);
+KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_5_none, o_rabk_5_shift, KC_5, KC_NO);
+KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_6_none, o_lbrc_6_shift, KC_6, KC_NO);
+KEY_OVERRIDE_SHIFT_OR_COMBO(o_lbrc_7_none, o_rbrc_7_shift, KC_7, KC_LBRC);
+KEY_OVERRIDE_SHIFT_OR_COMBO(o_rbrc_8_none, o_hash_8_shift, KC_8, KC_RBRC);
 KEY_OVERRIDE_SHIFT_OR_COMBO(o_lprn_9_none, o_lprn_9_shift, KC_9, KC_LPRN);
 KEY_OVERRIDE_SHIFT_OR_COMBO(o_rprn_0_none, o_rprn_0_shift, KC_0, KC_RPRN);
 
@@ -26,6 +26,7 @@ KEY_OVERRIDE_SHIFT(o_slsh_bsls, KC_SLSH, KC_BSLS);
 KEY_OVERRIDE_SHIFT(o_exlm_ques, KC_EXLM, KC_QUES);
 KEY_OVERRIDE_SHIFT(o_ampr_pipe, KC_AMPR, KC_PIPE);
 KEY_OVERRIDE_SHIFT(o_astr_perc, KC_ASTR, KC_PERC);
+KEY_OVERRIDE_SHIFT(o_hash_dlr,  KC_HASH, KC_DLR);
 
 const key_override_t *key_overrides[] = {
   &o_lcbr_1_none,
@@ -36,19 +37,20 @@ const key_override_t *key_overrides[] = {
   &o_dlr_3_shift,
   &o_rabk_4_none,
   &o_labk_4_shift,
-  &o_dlr_5_none,
+  &o_xxxx_5_none,
   &o_rabk_5_shift,
-  &o_lbrc_6_none,
+  &o_xxxx_6_none,
   &o_lbrc_6_shift,
-  &o_rbrc_7_none,
+  &o_lbrc_7_none,
   &o_rbrc_7_shift,
-  &o_hash_8_none,
+  &o_rbrc_8_none,
   &o_hash_8_shift,
   &o_lprn_9_none,
   &o_lprn_9_shift,
   &o_rprn_0_none,
   &o_rprn_0_shift,
 
+  &o_hash_dlr,
   &o_comm_circ,
   &o_dot_at,
   &o_slsh_bsls,
--- keymap.c
+++ keymap.c
@@ -12,9 +12,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_DEF] = LAYOUT_moonlander(
     // These numbers aren't actually numbers unless shifted. See key_overrides for more info.
-    //          KC_LCBR,    KC_RCBR,    KC_LABK,    KC_RABK,    KC_DLR,                                                     KC_LBRC,    KC_RBRC,    KC_HASH,    KC_LPRN,    KC_RPRN,
+    //          KC_LCBR,    KC_RCBR,    KC_LABK,    KC_RABK,    XXXXXXXX,                                                   XXXXXXXX,   KC_LBRC,    KC_RBRC,    KC_LPRN,    KC_RPRN,
     KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_CALC,                            KC_PSCR,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       OSL(L_FUN),
-    KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,                            KC_END,     KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       XXXXXXX,
+    KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,                            KC_END,     KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_HASH,
     KC_LGUI,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       TH_ALTU,                            TG(L_NUM),  KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_GRV,     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                                                       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_AMPR,
     XXXXXXX,    KC_MINS,    KC_EQL,     KC_UP,      KC_DOWN,                XXXXXXX,                            LCTL(KC_V),             KC_LEFT,    KC_RGHT,    KC_EXLM,    KC_ASTR,    KC_DEL,
