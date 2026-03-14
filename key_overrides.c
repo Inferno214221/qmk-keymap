ed84c80bd84130229966607112e6b2465f1c5a60
inferno214221 <inferno214221@gmail.com>
2026-03-14 21:44:16 +1030

    Moves asterisk to better but shifted position.

--- key_overrides.c
+++ key_overrides.c
@@ -20,12 +20,12 @@ KEY_OVERRIDE_SHIFT_OR_COMBO(o_rprn_0_none, o_rprn_0_shift, KC_0, KC_RPRN);
 #define KEY_OVERRIDE_SHIFT(NAME, FROM, TO) \
 const key_override_t NAME = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, FROM, TO, 1 << L_DEF, ~MOD_MASK_SHIFT);
 
-KEY_OVERRIDE_SHIFT(o_comm_circ, KC_COMM, KC_CIRC);
+KEY_OVERRIDE_SHIFT(o_comm_astr, KC_COMM, KC_ASTR);
 KEY_OVERRIDE_SHIFT(o_dot_at,    KC_DOT,  KC_AT);
 KEY_OVERRIDE_SHIFT(o_slsh_bsls, KC_SLSH, KC_BSLS);
-KEY_OVERRIDE_SHIFT(o_exlm_ques, KC_EXLM, KC_QUES);
+KEY_OVERRIDE_SHIFT(o_exlm_circ, KC_EXLM, KC_CIRC);
 KEY_OVERRIDE_SHIFT(o_ampr_pipe, KC_AMPR, KC_PIPE);
-KEY_OVERRIDE_SHIFT(o_astr_perc, KC_ASTR, KC_PERC);
+KEY_OVERRIDE_SHIFT(o_ques_perc, KC_QUES, KC_PERC);
 KEY_OVERRIDE_SHIFT(o_hash_dlr,  KC_HASH, KC_DLR);
 
 const key_override_t *key_overrides[] = {
@@ -51,10 +51,10 @@ const key_override_t *key_overrides[] = {
   &o_rprn_0_shift,
 
   &o_hash_dlr,
-  &o_comm_circ,
+  &o_comm_astr,
   &o_dot_at,
   &o_slsh_bsls,
-  &o_exlm_ques,
+  &o_exlm_circ,
   &o_ampr_pipe,
-  &o_astr_perc,
+  &o_ques_perc,
 };
