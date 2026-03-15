bca8c1d7d049f0eb8b152c3d29726a0f96e0915c
inferno214221 <inferno214221@gmail.com>
2026-03-15 23:36:58 +1030

    Switches to alternative layout with reduced movement.

--- key_overrides.c
+++ key_overrides.c
@@ -7,48 +7,48 @@ const key_override_t NAME_NONE = ko_make_with_layers_and_negmods(0, FROM, TO, 1 
 const key_override_t NAME_SHIFT = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, FROM, FROM, 1 << L_DEF, ~MOD_MASK_SHIFT);
 
 // KEY_OVERRIDE_SHIFT_OR_COMBO(o_lcbr_1_none, o_lcbr_1_shift, KC_1, KC_LCBR);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_rcbr_2_none, o_rcbr_2_shift, KC_2, KC_RCBR);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_rcbr_2_none, o_rcbr_2_shift, KC_2, KC_RCBR);
 // KEY_OVERRIDE_SHIFT_OR_COMBO(o_labk_3_none, o_dlr_3_shift,  KC_3, KC_LABK);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_rabk_4_none, o_labk_4_shift, KC_4, KC_RABK);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_5_none, o_rabk_5_shift, KC_5, KC_NO);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_6_none, o_lbrc_6_shift, KC_6, KC_NO);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_rabk_4_none, o_labk_4_shift, KC_4, KC_RABK);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_5_none, o_rabk_5_shift, KC_5, KC_NO);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_xxxx_6_none, o_lbrc_6_shift, KC_6, KC_NO);
 // KEY_OVERRIDE_SHIFT_OR_COMBO(o_lbrc_7_none, o_rbrc_7_shift, KC_7, KC_LBRC);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_rbrc_8_none, o_hash_8_shift, KC_8, KC_RBRC);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_rbrc_8_none, o_hash_8_shift, KC_8, KC_RBRC);
 // KEY_OVERRIDE_SHIFT_OR_COMBO(o_lprn_9_none, o_lprn_9_shift, KC_9, KC_LPRN);
-KEY_OVERRIDE_SHIFT_OR_COMBO(o_rprn_0_none, o_rprn_0_shift, KC_0, KC_RPRN);
+// KEY_OVERRIDE_SHIFT_OR_COMBO(o_rprn_0_none, o_rprn_0_shift, KC_0, KC_RPRN);
 
 #define KEY_OVERRIDE_SHIFT(NAME, FROM, TO) \
 const key_override_t NAME = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, FROM, TO, 1 << L_DEF, ~MOD_MASK_SHIFT);
 
-KEY_OVERRIDE_SHIFT(o_comm_astr, KC_COMM, KC_ASTR);
-KEY_OVERRIDE_SHIFT(o_dot_dot,   KC_DOT,  KC_DOT);
+KEY_OVERRIDE_SHIFT(o_comm_astr, KC_COMM, KC_QUES);
+KEY_OVERRIDE_SHIFT(o_dot_dot,   KC_DOT,  KC_EXLM);
 KEY_OVERRIDE_SHIFT(o_slsh_bsls, KC_SLSH, KC_BSLS);
-KEY_OVERRIDE_SHIFT(o_exlm_circ, KC_EXLM, KC_CIRC);
+KEY_OVERRIDE_SHIFT(o_exlm_circ, KC_ASTR, KC_HASH);
 KEY_OVERRIDE_SHIFT(o_ampr_pipe, KC_AMPR, KC_PIPE);
-KEY_OVERRIDE_SHIFT(o_ques_perc, KC_QUES, KC_AT);
-KEY_OVERRIDE_SHIFT(o_hash_dlr,  KC_HASH, KC_DLR);
+KEY_OVERRIDE_SHIFT(o_ques_perc, KC_CIRC, KC_AT);
+KEY_OVERRIDE_SHIFT(o_hash_dlr,  KC_DLR,  KC_PERC);
 
 const key_override_t *key_overrides[] = {
   // &o_lcbr_1_none,
   // &o_lcbr_1_shift,
-  &o_rcbr_2_none,
-  &o_rcbr_2_shift,
+  // &o_rcbr_2_none,
+  // &o_rcbr_2_shift,
   // &o_labk_3_none,
   // &o_dlr_3_shift,
-  &o_rabk_4_none,
-  &o_labk_4_shift,
-  &o_xxxx_5_none,
-  &o_rabk_5_shift,
-  &o_xxxx_6_none,
-  &o_lbrc_6_shift,
+  // &o_rabk_4_none,
+  // &o_labk_4_shift,
+  // &o_xxxx_5_none,
+  // &o_rabk_5_shift,
+  // &o_xxxx_6_none,
+  // &o_lbrc_6_shift,
   // &o_lbrc_7_none,
   // &o_rbrc_7_shift,
-  &o_rbrc_8_none,
-  &o_hash_8_shift,
+  // &o_rbrc_8_none,
+  // &o_hash_8_shift,
   // &o_lprn_9_none,
   // &o_lprn_9_shift,
-  &o_rprn_0_none,
-  &o_rprn_0_shift,
+  // &o_rprn_0_none,
+  // &o_rprn_0_shift,
 
   &o_hash_dlr,
   &o_comm_astr,
--- keymap.c
+++ keymap.c
@@ -11,33 +11,31 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_DEF] = LAYOUT_moonlander(
-    // These numbers aren't actually numbers unless shifted. See key_overrides for more info.
-    //          KC_LCBR,    KC_RCBR,    KC_LABK,    KC_RABK,    XXXXXXXX,                                                   XXXXXXXX,   KC_LBRC,    KC_RBRC,    KC_LPRN,    KC_RPRN,
-    KC_ESC,     PR_BRACE,   KC_2,       PR_ANGLE,   KC_4,       MA_ARROW,   KC_CALC,                            KC_PSCR,    MA_SLICE,   PR_BRACK,   KC_8,       PR_PAREN,   KC_0,       OSL(L_FUN),
-    KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,                            KC_END,     KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_HASH,
-    KC_LGUI,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       TH_ALTU,                            TG(L_NUM),  KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    PR_QUOTE,
+    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_CALC,                            KC_PSCR,    KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       OSL(L_FUN),
+    KC_DLR,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       XXXXXXX,                            LCTL(KC_V), KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_ASTR,
+    KC_CIRC,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       OSL(L_NUM),                         TD(T_COPY), KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    PR_QUOTE,
     PR_BTICK,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                                                       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    PR_AMPIP,
-    KC_PERC,    KC_MINS,    KC_EQL,     KC_UP,      KC_DOWN,                XXXXXXX,                            LCTL(KC_V),             KC_LEFT,    KC_RGHT,    KC_QUES,    KC_EXLM,    KC_DEL,
-                                                                KC_LSFT,    KC_BSPC,    KC_LCTL,    TD(T_COPY), KC_SPC,     KC_ENT
+    KC_TAB,     KC_MINS,    KC_EQL,     KC_UP,      KC_DOWN,                TH_ALTU,                            KC_LGUI,                KC_LEFT,    KC_RGHT,    KC_HOME,    KC_END,     KC_DEL,
+                                                                KC_LSFT,    KC_BSPC,    KC_LCTL,    XXXXXXX,    KC_SPC,     KC_ENT
   ),
   [L_FUN] = LAYOUT_moonlander(
     QK_BOOT,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                              KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     INF_NULL,
     XXXXXXX,    KC_VOLD,    KC_MPLY,    KC_VOLU,    XXXXXXX,    XXXXXXX,    XXXXXXX,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KM_PLAY,    XXXXXXX,    XXXXXXX,
-    XXXXXXX,    KC_MPRV,    KC_MSTP,    KC_MNXT,    XXXXXXX,    XXXXXXX,    _______,                            TO(L_GAM),  XXXXXXX,    XXXXXXX,    KM_PREV,    KM_STOP,    KM_NEXT,    XXXXXXX,
+    XXXXXXX,    KC_MPRV,    KC_MSTP,    KC_MNXT,    XXXXXXX,    XXXXXXX,    TO(L_GAM),                          XXXXXXX,    XXXXXXX,    XXXXXXX,    KM_PREV,    KM_STOP,    KM_NEXT,    XXXXXXX,
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-    AU_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                XXXXXXX,                            XXXXXXX,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    TO(L_TET),
+    AU_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                _______,                            XXXXXXX,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    TO(L_TET),
                                                                 _______,    XXXXXXX,    _______,    XXXXXXX,    KC_SLEP,    KC_PWR
   ),
   [L_NUM] = LAYOUT_moonlander(
-    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_CIRC,    KC_LPRN,    KC_RPRN,    KC_PSLS,
-    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            _______,    XXXXXXX,    XXXXXXX,    KC_P7,      KC_P8,      KC_P9,      KC_PAST,
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                            _______,    XXXXXXX,    XXXXXXX,    KC_P4,      KC_P5,      KC_P6,      KC_PMNS,
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                    XXXXXXX,    XXXXXXX,    KC_P1,      KC_P2,      KC_P3,      KC_PPLS,
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,    _______,                XXXXXXX,                            _______,                _______,    _______,    KC_P0,      KC_PDOT,    KC_PENT,
+    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+    _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KC_RCBR,    KC_RABK,    XXXXXXX,                            _______,    KC_RBRC,    KC_RPRN,    KC_P7,      KC_P8,      KC_P9,      KC_PAST,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    PR_BRACE,   PR_ANGLE,   _______,                            _______,    PR_BRACK,   PR_PAREN,    KC_P4,      KC_P5,      KC_P6,      KC_PSLS,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                    XXXXXXX,    XXXXXXX,    KC_P1,      KC_P2,      KC_P3,      KC_CIRC,
+    XXXXXXX,    KC_PMNS,    KC_PPLS,    _______,    _______,                XXXXXXX,                            XXXXXXX,                _______,    _______,    KC_P0,      KC_PDOT,    KC_PENT,
                                                                 XXXXXXX,    _______,    XXXXXXX,    _______,    _______,    _______
   ),
   [L_GAM] = LAYOUT_moonlander(
-    KC_ESC,     _______,    _______,    _______,    _______,    _______,    XXXXXXX,                            G(KC_PSCR), _______,    _______,    _______,    _______,    _______,    TO(L_DEF),
+    KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       XXXXXXX,                            G(KC_PSCR), KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       TO(L_DEF),
     KC_TAB,     _______,    _______,    _______,    _______,    _______,    XXXXXXX,                            XXXXXXX,    _______,    _______,    _______,    _______,    _______,    XXXXXXX,
     KC_GRV,     _______,    _______,    _______,    _______,    _______,    KC_LALT,                            TO(L_DEF),  _______,    _______,    _______,    _______,    KC_SCLN,    KC_QUOT,
     KC_LSFT,    _______,    _______,    _______,    _______,    _______,                                                    _______,    _______,    KC_COMM,    KC_DOT,     KC_UP,      KC_BSLS,
