a4d24cce304286648a9b2f3fda5e0005fc204084
inferno214221 <inferno214221@gmail.com>
2026-03-15 20:39:04 +1030

    Swaps question mark and exclamation mark.

--- keymap.c
+++ keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_HOME,                            KC_END,     KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_HASH,
     KC_LGUI,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       TH_ALTU,                            TG(L_NUM),  KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    PR_QUOTE,
     PR_BTICK,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                                                       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    PR_AMPIP,
-    KC_PERC,    KC_MINS,    KC_EQL,     KC_UP,      KC_DOWN,                XXXXXXX,                            LCTL(KC_V),             KC_LEFT,    KC_RGHT,    KC_EXLM,    KC_QUES,    KC_DEL,
+    KC_PERC,    KC_MINS,    KC_EQL,     KC_UP,      KC_DOWN,                XXXXXXX,                            LCTL(KC_V),             KC_LEFT,    KC_RGHT,    KC_QUES,    KC_EXLM,    KC_DEL,
                                                                 KC_LSFT,    KC_BSPC,    KC_LCTL,    TD(T_COPY), KC_SPC,     KC_ENT
   ),
   [L_FUN] = LAYOUT_moonlander(
