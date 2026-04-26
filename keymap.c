c965f926b311344ed8ce60bf4179a714293893aa
inferno214221 <inferno214221@gmail.com>
2026-04-26 11:06:33 +0930

    Adds indicator on all layers.

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_FUN] = LAYOUT_moonlander(
     QK_BOOT,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                              KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     INF_NUL,
     XXXXXXX,    KC_VOLD,    KC_MPLY,    KC_VOLU,    XXXXXXX,    XXXXXXX,    XXXXXXX,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    KM_PLAY,    XXXXXXX,    XXXXXXX,
-    XXXXXXX,    KC_MPRV,    KC_MSTP,    KC_MNXT,    XXXXXXX,    XXXXXXX,    _______,                            TO(L_GAM),  XXXXXXX,    XXXXXXX,    KM_PREV,    KM_STOP,    KM_NEXT,    XXXXXXX,
+    XXXXXXX,    KC_MPRV,    KC_MSTP,    KC_MNXT,    XXXXXXX,    XXXXXXX,    INF_IND,                            TO(L_GAM),  XXXXXXX,    XXXXXXX,    KM_PREV,    KM_STOP,    KM_NEXT,    XXXXXXX,
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
     AU_TOGG,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                _______,                            XXXXXXX,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    TO(L_TET),
                                                                 _______,    XXXXXXX,    _______,    XXXXXXX,    KC_SLEP,    KC_PWR
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NUM] = LAYOUT_moonlander(
     _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,    _______,    XXXXXXX,
     _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            _______,    XXXXXXX,    XXXXXXX,    KC_P7,      KC_P8,      KC_P9,      KC_PAST,
-    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    _______,                            _______,    XXXXXXX,    XXXXXXX,    KC_P4,      KC_P5,      KC_P6,      KC_PSLS,
+    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    INF_IND,                            _______,    XXXXXXX,    XXXXXXX,    KC_P4,      KC_P5,      KC_P6,      KC_PSLS,
     XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                                                    XXXXXXX,    XXXXXXX,    KC_P1,      KC_P2,      KC_P3,      KC_CIRC,
     XXXXXXX,    KC_PMNS,    KC_PPLS,     _______,    _______,               XXXXXXX,                            _______,                _______,    _______,    KC_P0,      KC_PDOT,    KC_PENT,
                                                                 XXXXXXX,    _______,    XXXXXXX,    _______,    _______,    _______
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_GAM] = LAYOUT_moonlander(
     KC_ESC,     _______,    _______,    _______,    _______,    _______,    XXXXXXX,                            G(KC_PSCR), _______,    _______,    _______,    _______,    _______,    TO(L_DEF),
     KC_TAB,     _______,    _______,    _______,    _______,    _______,    XXXXXXX,                            XXXXXXX,    _______,    _______,    _______,    _______,    _______,    XXXXXXX,
-    KC_GRV,     _______,    _______,    _______,    _______,    _______,    _______,                            TO(L_DEF),  _______,    _______,    _______,    _______,    KC_SCLN,    KC_QUOT,
+    KC_GRV,     _______,    _______,    _______,    _______,    _______,    INF_IND,                            TO(L_DEF),  _______,    _______,    _______,    _______,    KC_SCLN,    KC_QUOT,
     KC_LSFT,    _______,    _______,    _______,    _______,    _______,                                                    _______,    _______,    KC_COMM,    KC_DOT,     KC_UP,      KC_BSLS,
     KC_LCTL,    KC_F1,      KC_F2,      KC_F3,      KC_F4,                  KC_LALT,                            XXXXXXX,                KC_DEL,     KC_SLSH,    KC_LEFT,    KC_DOWN,    KC_RGHT,
                                                                 KC_SPC,     KC_SPC,     KC_LGUI,    XXXXXXX,    XXXXXXX,    XXXXXXX
